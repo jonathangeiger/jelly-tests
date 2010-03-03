c004528c823ccdbec2389a9113d4c1e9639f68f7
jonathangeiger <jonathan@jonathan-geiger.com>
2010-03-03 16:30:47 -0500

    Starting to add a few tests for issues that are anomolous

---
+++ tests/Jelly/Issues.php
@@ -0,0 +1,33 @@
+<?php
+
+Jelly_Test::bootstrap();
+
+/**
+ * Tests various issues that have come up
+ *
+ * @group jelly
+ * @group jelly.issues
+ */
+class Jelly_Issues extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Tests that passwords aren't rehashed when pulled from the database
+	 */
+	public function testIssue8()
+	{
+		$author = Jelly::factory('author');
+		$author->load_values(array('password' => '12345'));
+		$this->assertEquals('12345', $author->password);
+	}
+	
+	/**
+	 * Tests that in_db values that are set to the same value as their
+	 * original aren't registered as changed values.
+	 */
+	public function testIssue16()
+	{
+		$post = Jelly::select('post', 1);
+		$post->id = 1;
+		$this->assertEquals(FALSE, $post->changed('id'));
+	}
+}
