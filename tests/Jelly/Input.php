52dc89ebc004250b791f4e488f9b4e81202eadcd
Paul Banks <banks@banksdesigns.co.uk>
2010-03-05 18:41:43 +0000

    Tests for new helper based input views

--- tests/Jelly/Input.php
+++ tests/Jelly/Input.php
@@ -14,13 +14,15 @@ Class Jelly_Input extends PHPUnit_Framework_TestCase
 	{
 		// We'll use this for constructing the HTML
 		$model = Jelly::select('post', 1);
-		$expected = $model->input('author')->render();
+		$expected = $model->input('author')->render();		
 		
 		// And mock construct another one to test against
 		$view = View::factory('jelly/field/belongsto');
-		$view->name = 'author';
-		$view->foreign = array('model' => 'author');
-		$view->value = Jelly::select('author', 1);
+		$view->name			= 'author';
+		$view->foreign		= array('model' => 'author');
+		$view->value		= Jelly::select('author', 1);
+		$view->options		= array(1 => 'Jonathan Geiger');
+		$view->attributes	= array();
 		
 		// And compare
 		$this->assertEquals($expected, $view->render());
@@ -34,9 +36,14 @@ Class Jelly_Input extends PHPUnit_Framework_TestCase
 		
 		// And mock construct another one to test against
 		$view = View::factory('jelly/field/hasone');
-		$view->name = 'post';
-		$view->foreign = array('model' => 'post');
-		$view->value = Jelly::select('post', 1);
+		$view->name			= 'post';
+		$view->foreign		= array('model' => 'post');
+		$view->value		= Jelly::select('post', 1);
+		$view->options		= array(
+			1 => 'First Post',
+			2 => 'Second Post',
+		);
+		$view->attributes	= array();
 		
 		// And compare
 		$this->assertEquals($expected, $view->render());
@@ -50,9 +57,10 @@ Class Jelly_Input extends PHPUnit_Framework_TestCase
 		
 		// And mock construct another one to test against
 		$view = View::factory('jelly/field/hasmany');
-		$view->name = 'posts';
-		$view->foreign = array('model' => 'post');
-		$view->ids = Jelly::select('post')->execute()->as_array(NULL, 'id');
+		$view->name			= 'posts';
+		$view->foreign		= array('model' => 'post');
+		$view->ids			= Jelly::select('post')->execute()->as_array(NULL, 'id');
+		$view->attributes	= array();
 		
 		// And compare
 		$this->assertEquals($expected, $view->render());
@@ -66,9 +74,30 @@ Class Jelly_Input extends PHPUnit_Framework_TestCase
 		
 		// And mock construct another one to test against
 		$view = View::factory('jelly/field/manytomany');
-		$view->name = 'categories';
-		$view->foreign = array('model' => 'category');
-		$view->ids = Jelly::select('category')->execute()->as_array(NULL, 'id');
+		$view->name			= 'categories';
+		$view->foreign		= array('model' => 'category');
+		$view->ids			= Jelly::select('category')->execute()->as_array(NULL, 'id');
+		$view->attributes	= array();
+		
+		// And compare
+		$this->assertEquals($expected, $view->render());
+	}
+	
+	public function testEnum()
+	{
+		// We'll use this for constructing the HTML
+		$model = Jelly::select('post', 1);
+		
+		// Also test adding extra attributes
+		$expected = $model->input('status', array('attributes' => array('class' => 'test')))->render();
+		
+		// And mock construct another one to test against
+		$view = View::factory('jelly/field/enum');
+		$view->name			= 'status';
+		$view->value		= 'draft';
+		$view->attributes	= array('class' => 'test');
+		$view->choices		= array('published', 'draft', 'review');
+		
 		// And compare
 		$this->assertEquals($expected, $view->render());
 	}
