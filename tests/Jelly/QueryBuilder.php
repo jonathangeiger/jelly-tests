8b656e6bf61db3f6a8b4d508c62e7916a1ae4a37
jonathangeiger <jonathan@jonathan-geiger.com>
2010-03-09 13:56:26 -0500

    Updating tests to reflect the new as_array() support

--- tests/Jelly/QueryBuilder.php
+++ tests/Jelly/QueryBuilder.php
@@ -13,37 +13,23 @@ class Jelly_QueryBuilder extends PHPUnit_Framework_TestCase
 	public function providerQueryBuildingProducesCorrectSQL()
 	{
 		return array(
-			array(Jelly::select('post'),
-				  'SELECT * FROM `posts`'),
-			array(Jelly::select('post')->where(':primary_key', '=', 1),
-				  'SELECT * FROM `posts` WHERE `posts`.`id` = 1'),
-			array(Jelly::select('post')->order_by(':primary_key', 'ASC'),
-				  'SELECT * FROM `posts` ORDER BY `posts`.`id` ASC'),
-			array(Jelly::select('author')->order_by('_id', 'ASC'),
-				  'SELECT * FROM `authors` ORDER BY `authors`.`id` ASC'),				
-			array(Jelly::select('author')->from('post'),
-				  'SELECT * FROM `authors`, `posts`'),				
-			array(Jelly::select('author')->with('role'),
-				  'SELECT `authors`.*, `roles`.`id` AS `:role:id`, '.
-				  '`roles`.`name` AS `:role:name` FROM `authors` '.
-				  'LEFT JOIN `roles` ON (`authors`.`role_id` = '. 
-				  '`roles`.`id`)'),
+			array(Jelly::select('post')),
+			array(Jelly::select('post')->where(':primary_key', '=', 1)),
+			array(Jelly::select('post')->order_by(':primary_key', 'ASC')),
+			array(Jelly::select('author')->order_by('_id', 'ASC')),				
+			array(Jelly::select('author')->from('post')),				
+			array(Jelly::select('author')->with('role')),
 			// This does not resolve to any model, but should still work
 			array(Jelly::select('categories_posts')
-			      ->where('post:foreign_key', '=', 1),
-				 'SELECT * FROM `categories_posts` WHERE `categories_posts`.`post_id` = 1')
+			      ->where('post:foreign_key', '=', 1))
 		);
 	}
 	
 	/**
 	 * @dataProvider providerQueryBuildingProducesCorrectSQL
 	 */
-	public function testQueryBuildingProducesCorrectSQL($query, $sql)
+	public function testQueryBuildingProducesCorrectSQL($query)
 	{
-		// Compile the SQL
-		$compiled = $query->compile(Database::instance(Jelly_Test::GROUP));
-		$this->assertEquals($compiled, $sql);
-		
 		// Execute the query and make sure no errors are thrown
 		// We have to manually specify the group because some
 		// queries are not attached to a model and use the wrong group
